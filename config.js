d422f383c2381d010d4e5e5f8dbfc73d49327ad2
flavioalejandrov24-lang <flavioalejandrov24@gmail.com>
2026-01-03 16:28:48 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 window.SUPABASE_CONFIG = {
   url: 'https://fmomggmoqvxysofxtykq.supabase.co',
-  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtb21nZ21vcXZ4eXNvZnh0eWtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5NzQwOTIsImV4cCI6MjA4MjU1MDA5Mn0.JB2DzJfQj_QQYlR1wKNw4mx2x3B3sDXMGLyABfDS_XQ'
+  key: 'sb_publishable_HZqSunHIRhHhAaw_kRz-zA_ZktUoSdP'
 };
