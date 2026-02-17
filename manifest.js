12e229794f33ae9c1a6a6b34e1bb5cee862fa665
flavioalejandrov24-lang <flavioalejandrov24@gmail.com>
2026-02-17 15:24:34 -0600

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,16 @@
+{
+  "name": "Personal Management Links",
+  "short_name": "MyLinks",
+  "start_url": "https://flavioalejandrov24-lang.github.io/Personalmanagmentlinks/",
+  "display": "standalone",
+  "background_color": "#000000",
+  "theme_color": "#000000",
+  "icons": [
+    {
+      "src": "https://www.pwabuilder.com/assets/icons/icon_192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any"
+    }
+  ]
+    }
